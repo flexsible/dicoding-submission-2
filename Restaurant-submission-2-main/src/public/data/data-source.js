b13b761bdbd360877420e2ef54f12833cd618143
Muhammad Rifqi Syamsul Ma'arif <39462524+flexsible@users.noreply.github.com>
2023-11-16 23:55:34 +0700

    Add files via upload

---
+++ Restaurant-submission-2-main/src/public/data/data-source.js
@@ -0,0 +1,17 @@
+// import data from './DATA.json';
+
+// data.restaurants.forEach(restaurant => {
+//     const listRestaurantElement = document.querySelector('#restaurantList');
+
+//     listRestaurantElement.innerHTML += `
+//     <div class="restaurantContainer" id="restaurantContainer" tabindex="0">
+//         <div class="card" id="card">
+//             <div id="(${restaurant.id})" class="card-body" >
+//             <img src="${restaurant.pictureId}" alt="${restaurant.name}">
+//             <h3>${restaurant.name}</h3>
+//             <p>${restaurant.description}</p>
+//             </div>
+//         </div>
+//     </div>
+//     `;
+// })
---
+++ Restaurant-submission-2-main/src/scripts/views/app.js
@@ -0,0 +1,17 @@
+import urlParser from "../routes/url-parser";
+import routes from "../routes/routes";
+
+class App {
+    constructor({ content }) {
+        this._content = content;
+    }
+
+    async renderPage(){
+        const url = urlParser.parseActiveUrlWithCombiner();
+        const page = routes[url];
+        this._content.innerHTML = await page.render();
+        await page.afterRender();
+    }
+}
+
+export default App;
